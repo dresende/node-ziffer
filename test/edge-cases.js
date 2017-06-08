e6667300486b6b85e3da7855c83263e4f0e6cd6b
Diogo Resende <dresende@thinkdigital.pt>
2017-06-08 22:35:59 +0100

    test: adds more edge cases

--- test/edge-cases.js
+++ test/edge-cases.js
@@ -8,6 +8,7 @@ describe("Edge Cases", () => {
 		n.format(NaN).should.equal("-");
 		n.format("what").should.equal("-");
 		n.format("3.01").should.equal("3,01");
+		n.format("0.0000").should.equal("0");
 	});
 
 	it("Should return '-' for infinite numbers", () => {
