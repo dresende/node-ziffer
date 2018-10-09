0e35d7d530dc5fa390eeb4364473505a99d30261
Diogo Resende <dresende@thinkdigital.pt>
2018-10-09 15:11:01 +0100

    fixes nasty little bug on precision rounding (example: 9,945 to 1 decimal would throw instead of returning 9,9)

--- index.js
+++ index.js
@@ -268,5 +268,5 @@ function precision_round(number, decimals) {
 	let p1 = Math.pow(10, decimals + 1);
 	let p2 = Math.pow(10, decimals);
 
-	return "" + Math.round( Math.round( number * p1 ) / (p1 / p2) ) / p2;
+	return "" + (Math.round( Math.floor( number * p1 ) / (p1 / p2) ) / p2);
 }
--- test/options.js
+++ test/options.js
@@ -11,5 +11,13 @@ describe("Options", () => {
 		n.format(12.3456, { decimals: 3 }).should.equal("12,346");
 		// default
 		n.format(12.3456).should.equal("12,35");
+
+		// nasty edge cases
+		n.format(9.945, { decimals: 0 }).should.equal("10");
+		n.format(9.945, { decimals: 1 }).should.equal("9,9");
+		n.format(9.945, { decimals: 2 }).should.equal("9,95");
+		n.format(9.945, { decimals: 3 }).should.equal("9,945");
+		n.format(9.945, { decimals: 4 }).should.equal("9,9450");
+		n.format(9.945, { decimals: 5 }).should.equal("9,94500");
 	});
 });
